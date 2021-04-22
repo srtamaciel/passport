0607fdf563babcf6fd6b4b8b3766c86feb94d9a2
Raquel Rodriguez <srtademaciel@gmail.com>
2021-04-22 10:04:14 +0200

    link

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const User = require('./models/User.model')
 
 // ---------------------DB CONFIG MONGOOSE--------------------
 mongoose
-  .connect('mongodb://localhost/passport-example', {
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5f76.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
